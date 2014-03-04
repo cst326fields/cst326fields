308c70ca4173710d04e8f1b66d5508e15235d3c7
Logan <kmartshopper@gmail.com>
2014-03-04 07:06:11 -0800

    Index nearly looks done.

--- MVC App/MVC App/App_Start/BundleConfig.cs
+++ MVC App/MVC App/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace MVC_App
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      /*"~/Content/bootstrap.css",*/
+                      /*"~/Content/site.css",*/
                       "~/Content/aes_styles.css"));
         }
     }
