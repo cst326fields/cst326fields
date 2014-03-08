594c5dd7aeaa9e34086426a70b7ce5c4dca310e7
loganwrightoit <logan.wright@oit.edu>
2014-03-07 17:06:49 -0800

    Start of filling personal info form.

--- AESApplications/AESApplications/Controllers/PersonalInfoController.cs
+++ AESApplications/AESApplications/Controllers/PersonalInfoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AESApplications.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,7 +13,9 @@ namespace AESApplications.Controllers
         // GET: /PersonalInfo/
         public ActionResult Index()
         {
-            return View();
+            
+
+            return View(new PersonalInfoModel());
         }
 	}
 }
