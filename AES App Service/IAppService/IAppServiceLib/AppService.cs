e34de46fce0c4d9460217e0e28c905c2b1182bbe
Dustin Falgout <dustin.falgout@oit.edu>
2014-03-07 16:15:42 -0800

    Added Fresh WCF IAppService Projects
    AppServiceLib, AppServiceHost, AppServiceTestClient

---
+++ AES App Service/IAppService/IAppServiceLib/AppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IAppServiceLib
+{
+    public class AppService : IAppService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
