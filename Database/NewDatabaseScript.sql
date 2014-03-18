d1d5f01a1b8ff483294a0933ec52af70ba2f38c3
Boese <christopher.boese@oit.edu>
2014-03-18 14:59:04 -0700

    Fixed Database Education-> graduated field

--- Database/NewDatabaseScript.sql
+++ Database/NewDatabaseScript.sql
@@ -116,7 +116,7 @@ CREATE TABLE [dbo].[Education]
     CONSTRAINT [FK_PersonalInfo_Education] FOREIGN KEY (applicantId) REFERENCES [dbo].[PersonalInfo] ([applicantId]), 
     [nameAddress] VARCHAR(255) NULL, 
     [yearsAttended] VARCHAR(255) NULL, 
-    [graduated] CHAR(1) NULL, 
+    [graduated] VARCHAR(16) NULL, 
     [degreeMajor] VARCHAR(255) NULL,
 	PRIMARY KEY CLUSTERED ([applicantId] ASC, [educationId] ASC)
 );
